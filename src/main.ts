07bd14a3d4cef82954a002230683e37282ec4327
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-03 15:00:13 +0200

    Create html structure with createElement function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './style.css';
+import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const contentSection = document.createElement('main');
+/* const contentSection = document.createElement('main');
 const headerElement = document.createElement('header');
 headerElement.className = 'header';
 const h1Element = document.createElement('h1');
@@ -11,8 +12,30 @@ h1Element.innerText = 'Rick and Morty';
 headerElement.append(h1Element);
 const InputElement = document.createElement('input');
 InputElement.placeholder = 'Type something in';
-contentSection.append(InputElement);
+contentSection.append(InputElement); */
+
+const page = createElement('div', {
+  className: 'wrapper',
+  childElements: [
+    createElement('header', {
+      className: 'header',
+      childElements: [
+        createElement('h1', {
+          className: 'header__headline',
+          innerText: 'Rick and Morty',
+        }),
+      ],
+    }),
+    createElement('main', {
+      childElements: [
+        createElement('input', {
+          placeholder: 'Type something in',
+        }),
+      ],
+    }),
+  ],
+});
 
 if (app !== null) {
-  app.append(headerElement, contentSection);
+  app.append(page);
 }
