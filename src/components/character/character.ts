2dcc25925e1bcc6b7dcab71367bdefd685777341
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-04 09:02:10 +0200

    Add an innerText attribute in created h2 element in character.ts. Make some CSS adjustments für the character card

--- src/components/character/character.ts
+++ src/components/character/character.ts
@@ -14,6 +14,7 @@ export function createCharacterCard() {
         childElements: [
           createElement('h2', {
             className: 'character__name',
+            innerText: 'Funny guy',
           }),
           createElement('section', {
             className: 'character__aliveStatus',
