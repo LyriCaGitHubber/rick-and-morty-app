0c714221dde27a6d75f29bbf815068e547702434
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-05 16:16:45 +0200

    Create api.ts to get characters and create CharacterFromAPI typescript types

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,18 @@
-export async function getCharacters() {
+import { AllCharactersFromAPI, Character } from '../../src/types';
+
+export async function getCharacters(): Promise<Character[]> {
   const response = await fetch('https://rickandmortyapi.com/api/character');
-  const data = await response.json();
-  const results = data.results;
-  return results;
+  const data: AllCharactersFromAPI = await response.json();
+  const characters = data.results;
+  const formattedCharacters: Character[] = characters.map((character) => {
+    const formattedCharacter: Character = {
+      name: character.name,
+      origin: character.origin.name,
+      species: character.species,
+      status: character.status,
+      thumbnail: character.image,
+    };
+    return formattedCharacter;
+  });
+  return formattedCharacters;
 }
